15824f8a59f4f56b382987398671651913db7257
omarzouaoui <zouaouiomar80@gmail.com>
2022-11-16 12:32:08 +0100

    done

---
+++ warmup01.js
@@ -0,0 +1,16 @@
+// 1-using+operator create a function that combine your partner first and last name and return it.
+// 2-create a function that return true if the number 13 is a multiple of 3 and false if not.
+// 4-create a function that takes a age as a parametre and return that age in seconds.
+
+// your code is here
+function combine(fName,sName){
+    return fName+" "+sName
+}
+function x(){
+    if(13%3===0){
+        return true
+    }return false
+}
+function age(age){
+    return age*365*24*60*60
+}
